d5c40827a976330c8856041c30241093b38e669d
nhnb <nhnb>
2009-09-20 09:41:36 +0000

    added comments

--- tests/net/sf/ircappender/app/TestApplication.java
+++ tests/net/sf/ircappender/app/TestApplication.java
@@ -24,19 +24,32 @@ public class TestApplication {
 	}
 
 	/**
-	 * @param args
-	 * @throws IOException 
-	 * @throws InterruptedException 
+	 * Starts this simple test application
+	 *
+	 * @param args ignored
+	 * @throws IOException in case of an input/output error 
+	 * @throws InterruptedException in case the thread is interrupted
 	 */
 	public static void main(String[] args) throws IOException, InterruptedException {
+
+		// init the log system
 		init();
 		Logger logger = Logger.getLogger(TestApplication.class);
+
+		// log errors and warnings
 		logger.fatal("fatal");
 		logger.error("error");
 		logger.warn("warn");
+
+		// log info messages
 		logger.info("info");
+
+		// Note: min level is set to "info" in log4j.properties, 
+		//       so there should not be any debug output in the channel
 		logger.debug("debug");
-		Thread.sleep(20000);
+
+		// wait a little and shutdown the log system
+		Thread.sleep(5000);
 		LogManager.shutdown();
 	}
 
